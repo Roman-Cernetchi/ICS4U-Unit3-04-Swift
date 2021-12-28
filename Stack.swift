8eb52edb3ac31e8c936e78b428597dd4299aac08
Ubuntu <ubuntu@ip-172-31-91-135.ec2.internal>
2021-12-28 03:36:42 +0000

    stack files part 4

---
+++ Stack.swift
@@ -0,0 +1,48 @@
+//
+//  Stack.swift
+//
+//  Created by Roman Cernetchi
+//  Created on 2021-12-27
+//  Version 1.0
+//  Copyright (c) 2021 Roman Cernetchi. All rights reserved.
+//
+//  This is the stack class.
+//
+// Class that emulates a stack
+class Stack<T> {
+
+    // Custom error types
+    enum Errors: Error { case emptyStackException }
+
+    // ArrayList signifying a stack
+    private var stackAsArray = [T]()
+
+    // Pushes a number to the stack
+    func push(pushElement: T) { self.stackAsArray.insert(pushElement, at: 0) }
+
+    // Pops the first element in the stack
+    func pop() throws -> T {
+        if self.stackAsArray.count == 0 {
+            throw Errors.emptyStackException
+        } else {
+            return self.stackAsArray.removeFirst()
+        }
+    }
+
+    // Returns the first element in the stack
+    func peek() throws -> T {
+        if self.stackAsArray.count == 0 {
+            throw Errors.emptyStackException
+        } else {
+            return self.stackAsArray[0]
+        }
+    }
+
+    // Clears the stack
+    func clear() { self.stackAsArray.removeAll() }
+
+    // Displays the stack
+    func showStack() {
+        self.stackAsArray.forEach { element in print("\(element)")}
+    }
+}
---
+++ main.swift
@@ -0,0 +1,56 @@
+//
+//  main.swift
+//
+//  Created by Roman Cernetchi
+//  Created on 2021-12-27
+//  Version 1.0
+//  Copyright (c) 2021 Roman Cernetchi. All rights reserved.
+//
+//  This program demonstrates a stack.
+//
+import Foundation
+
+// Takes user input in a loop and permits the user to make multiple commands
+// exit = end of program
+let aStack = Stack<Int>()
+
+let allowedCommands = ["push", "pop", "show", "peek", "clear", "exit"]
+print("Acceptable Commands:\npush <number> - pop - show - peek - clear - exit")
+
+while true {
+    print("> ", terminator: "")
+    let userInput = readLine()
+
+    if userInput != nil {
+        if userInput!.starts(with: allowedCommands[0]) {
+            let userCommandArray = userInput!.components(separatedBy: " ")
+            if let userNumber = Int(userCommandArray[1]) {
+                aStack.push(pushElement: userNumber)
+            } else {
+                print("Please enter a valid command.")
+            }
+        } else if userInput!.starts(with: allowedCommands[1]) {
+            do {
+                try print("Removed: \(aStack.pop())")
+            } catch {
+                print("The stack is empty.")
+            }
+        } else if userInput!.starts(with: allowedCommands[2]) {
+            aStack.showStack()
+        } else if userInput!.starts(with: allowedCommands[3]) {
+            do {
+                try print("\(aStack.peek())")
+            } catch {
+                print("The stack is empty.")
+            }
+        } else if userInput!.starts(with: allowedCommands[4]) {
+            aStack.clear()
+        } else if userInput!.starts(with: allowedCommands[5]) {
+            break
+        } else {
+            print("Please enter a valid command.")
+        }
+    }
+}
+
+print("\nDone.")
